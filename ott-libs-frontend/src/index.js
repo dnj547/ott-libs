346df10f5a388275d143f2fa39b9523cf47e59ed
ferryrules <ferris@ferryrules.com>
2019-05-26 16:56:32 -0400

    play button

--- ott-libs-frontend/src/index.js
+++ ott-libs-frontend/src/index.js
@@ -1,19 +1,31 @@
 const userName = document.querySelector('#user-name')
+const user = document.querySelector('#uname')
+const submitUser = document.querySelector('#submit-user')
 USERS_URL = "http://localhost:3000/api/v1/users"
 STORIES_URL = "http://localhost:3000/api/v1/stories"
 TEMPLATES_URL = "http://localhost:3000/api/v1/templates"
 RECAPS_URL = "http://localhost:3000/api/v1/recaps"
 
-fetch(RECAPS_URL)
-  .then(r=>r.json())
-  .then(recaps=>{
-    recaps.forEach(recap=>{
-      userName.innerHTML +=`
-        <ul>
-          <li>${recap.users[0].name}</li>
-          <li>${recap.stories.map(story=>{
-            return story.words}).join(" ")}</li>
-        </ul>
-      `
+submitUser.addEventListener('click',e=>{
+
+  fetch(RECAPS_URL)
+    .then(r=>r.json())
+    .then(recaps=>{
+      recaps.forEach(recap=>{
+        if (recap.users[0].name === user.value) {
+          userName.innerHTML +=`
+            <ul>
+              <li>${recap.users[0].name}</li>
+              <li>${recap.stories.map(story=>{
+                return story.words}).join(" ")}
+              </li>
+            </ul>
+            <br>
+            <br>
+            <input type="submit" value="Play" id="play-game">
+          `
+        }
+      })
     })
-  })
+
+})
