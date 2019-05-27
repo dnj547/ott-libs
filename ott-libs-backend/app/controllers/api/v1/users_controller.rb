3419d0667fe1eda4bb27cf224d308ee1d274ae75
ferryrules <ferris@ferryrules.com>
2019-05-27 16:33:26 -0400

    add user/grab old user/simple play

--- ott-libs-backend/app/controllers/api/v1/users_controller.rb
+++ ott-libs-backend/app/controllers/api/v1/users_controller.rb
@@ -4,9 +4,23 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
   end
 
+  # def new
+  #   @user = User.new
+  # end
+
+  def create
+    @user = User.create(name: params[:name])
+  end
+
   def show
     @user = User.find(params[:name])
     render json: @user
   end
+  #
+  # private
+  #
+  # def user_params
+  #   params.require(:name)
+  # end
 
 end
--- ott-libs-frontend/src/index.js
+++ ott-libs-frontend/src/index.js
@@ -3,45 +3,43 @@ STORIES_URL = "http://localhost:3000/api/v1/stories"
 TEMPLATES_URL = "http://localhost:3000/api/v1/templates"
 RECAPS_URL = "http://localhost:3000/api/v1/recaps"
 
-const bodyYo = document.querySelector('#body-yo')
-const userName = document.querySelector('#user-name')
+const frontPage = document.querySelector('#front-page')
+const userForm = document.querySelector('#user-form')
+const signIn = document.querySelector('#signin')
 const user = document.querySelector('#uname')
-const submitUser = document.querySelector('#submit-user')
+const userRecaps = document.querySelector('#user-recaps')
 const template1 = document.querySelector('#template-1')
 const temp1Form = document.querySelector('#temp1-form')
 const playGame = document.querySelector('#play-game')
+let allUsers = []
+
+fetch(USERS_URL)
+  .then(r=>r.json())
+  .then(users=>{
+    users.forEach(user=>{
+      allUsers.push(user.name)
+    })
+  })
+
 
 template1.style.display = 'none'
 playGame.style.display = 'none'
 
-submitUser.addEventListener('click',e=>{
-
-  fetch(RECAPS_URL)
-    .then(r=>r.json())
-    .then(recaps=>{
-      recaps.forEach(recap=>{
-        if (recap.users[0].name === user.value) {
-          userName.innerHTML +=`
-            <ul>
-              <li>${recap.users[0].name}</li>
-              <li>${recap.stories.map(story=>{
-                return story.words}).join(" ")}
-              </li>
-            </ul>
-          `
-        }
-        playGame.style.display = ''
-      })
-    })
-
+userForm.addEventListener('submit',e=>{
+  e.preventDefault()
+  if (allUsers.includes(user.value)) {
+    listUserRecaps()
+  } else {
+    newUser()
+  }
 })
 
 playGame.addEventListener('click', e=>{
   user.style.display = 'none'
-  submitUser.style.display = 'none'
   playGame.style.display = 'none'
-  userName.style.display = 'none'
+  signIn.style.display = 'none'
   template1.style.display = ''
+
 })
 
 temp1Form.addEventListener('submit',e=>{
@@ -49,3 +47,38 @@ temp1Form.addEventListener('submit',e=>{
   temp1Form.style.color = "black"
   console.log(e.target.children[0].children[0].value);
 })
+
+// HELPER FETCHES
+function newUser() {
+  fetch(USERS_URL,{
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': user.value
+    })
+  })
+  playGame.style.display = ''
+}
+
+function listUserRecaps() {
+  fetch(RECAPS_URL)
+    .then(r=>r.json())
+    .then(recaps=>{
+      recaps.forEach(recap=>{
+        if (recap.users[0].name === user.value) {
+          userRecaps.innerHTML +=`
+          <ul>
+          <li>${recap.users[0].name}</li>
+          <li>${recap.stories.map(story=>{
+            return story.words}).join(" ")}
+            </li>
+            </ul>
+            `
+          }
+          playGame.style.display = ''
+        })
+    })
+}
